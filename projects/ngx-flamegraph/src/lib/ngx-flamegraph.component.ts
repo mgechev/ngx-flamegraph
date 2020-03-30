512dfc3ac7de3441f7efb941d759b483a3743cba
mgechev <mgechev@gmail.com>
2020-03-30 12:14:21 -0700

    feat: use OnPush for the component

--- projects/ngx-flamegraph/src/lib/ngx-flamegraph.component.ts
+++ projects/ngx-flamegraph/src/lib/ngx-flamegraph.component.ts
@@ -1,10 +1,11 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output, ChangeDetectionStrategy} from '@angular/core';
 import {Data, RawData, transformRawData, maxValue, SiblingLayout} from './utils';
 
 @Component({
   selector: 'ngx-flamegraph',
   templateUrl: './ngx-flamegraph.component.html',
-  styleUrls: ['./ngx-flamechart.component.css']
+  styleUrls: ['./ngx-flamechart.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NgxFlamegraphComponent {
   entries: Data[] = [];
