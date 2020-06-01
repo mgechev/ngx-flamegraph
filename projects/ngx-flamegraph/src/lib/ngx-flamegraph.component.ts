1800997d250ba0d030cfde094cc5962746309ebc
mgechev <mgechev@gmail.com>
2020-06-01 15:22:45 -0700

    Update npm version

--- projects/ngx-flamegraph/src/lib/ngx-flamegraph.component.ts
+++ projects/ngx-flamegraph/src/lib/ngx-flamegraph.component.ts
@@ -12,23 +12,36 @@ export class NgxFlamegraphComponent {
   entries: Data[] = [];
   depth = 0;
 
+  // tslint:disable-next-line: variable-name
+  private _colors: FlamegraphColor;
+  // tslint:disable-next-line: variable-name
+  private _data: RawData[];
+
   @Output() frameClick = new EventEmitter<RawData>();
   @Output() frameMouseEnter = new EventEmitter<RawData>();
   @Output() frameMouseLeave = new EventEmitter<RawData>();
 
-  @Input() colors: FlamegraphColor;
   @Input() siblingLayout: SiblingLayout = 'relative';
   @Input() width: number;
   @Input() levelHeight = 25;
-  @Input() set data(roots: RawData[]) {
-    const { hue, saturation, lightness } = this.colors ?? defaultColors;
+  @Input() set colors(colors: FlamegraphColor) {
+    this._colors = colors;
+    this._setData();
+  }
+  @Input() set data(data: RawData[]) {
+    this._data = data;
+    this._setData();
+  }
+
+  private _setData() {
+    const { hue, saturation, lightness } = this._colors ?? defaultColors;
     const colors: Color = {
       hue: Array.isArray(hue) ? hue : [hue, hue],
       saturation: Array.isArray(saturation) ? saturation : [saturation, saturation],
       lightness: Array.isArray(lightness) ? lightness : [lightness, lightness]
     };
-    this.entries = transformRawData(roots, this.siblingLayout, maxValue(roots), colors);
-    this.depth = findDepth(roots);
+    this.entries = transformRawData(this._data, this.siblingLayout, maxValue(this._data), colors);
+    this.depth = findDepth(this._data);
   }
 }
 
