b67b961803ef3859c651e508ee48c8dd54f8a810
mgechev <mgechev@gmail.com>
2020-11-22 21:27:45 +0200

    Update to v11

--- projects/ngx-flamegraph/src/lib/ngx-flamegraph.component.spec.ts
+++ projects/ngx-flamegraph/src/lib/ngx-flamegraph.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NgxFlamegraphComponent } from './ngx-flamegraph.component';
 
@@ -6,7 +6,7 @@ describe('NgxFlamegraphComponent', () => {
   let component: NgxFlamegraphComponent;
   let fixture: ComponentFixture<NgxFlamegraphComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NgxFlamegraphComponent ]
     })
