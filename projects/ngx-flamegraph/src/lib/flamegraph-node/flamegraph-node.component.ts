7ac8692cf71fe0de28f4fa3d9a92de16b62e7ebb
Enea Jahollari <jahollarienea14@gmail.com>
2022-04-08 19:01:12 +0200

    perf(flamegraph): listen to mouseover and mouseleave outside angular zone

--- projects/ngx-flamegraph/src/lib/flamegraph-node/flamegraph-node.component.ts
+++ projects/ngx-flamegraph/src/lib/flamegraph-node/flamegraph-node.component.ts
@@ -1,17 +1,61 @@
-import { Component, EventEmitter, Input, Output, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
-import {Data} from '../utils';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+  Renderer2,
+  ElementRef,
+  NgZone,
+  OnDestroy,
+} from "@angular/core";
+import { Data } from "../utils";
 
 @Component({
   // tslint:disable-next-line:component-selector
-  selector: '[flamegraph-node]',
-  templateUrl: './flamegraph-node.component.html',
-  styleUrls: ['./flamegraph-node.component.css'],
+  selector: "[flamegraph-node]",
+  templateUrl: "./flamegraph-node.component.html",
+  styleUrls: ["./flamegraph-node.component.css"],
   encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FlamegraphNodeComponent {
+export class FlamegraphNodeComponent implements OnDestroy {
   @Input() entry: Data;
   @Input() height: number;
   @Input() width: number;
   @Output() zoom = new EventEmitter();
+
+  @Output() mouseOverZoneless = new EventEmitter();
+  @Output() mouseLeaveZoneless = new EventEmitter();
+
+  private mouseOverTeardownFn: Function;
+  private mouseLeaveTeardownFn: Function;
+
+  constructor(
+    private _ngZone: NgZone,
+    private _element: ElementRef,
+    private _renderer: Renderer2
+  ) {}
+
+  ngOnInit(): void {
+    this._ngZone.runOutsideAngular(() => {
+      this.mouseOverTeardownFn = this._renderer.listen(
+        this._element.nativeElement,
+        "mouseover",
+        (event: MouseEvent) => this.mouseOverZoneless.emit(event)
+      );
+
+      this.mouseLeaveTeardownFn = this._renderer.listen(
+        this._element.nativeElement,
+        "mouseleave",
+        (event: MouseEvent) => this.mouseLeaveZoneless.emit(event)
+      );
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.mouseOverTeardownFn();
+    this.mouseLeaveTeardownFn();
+  }
 }
