623376e17f02030f0524c4a3a311bb464666ca96
hawkgs <hawk.dvp@gmail.com>
2025-05-14 23:54:57 +0300

    perf(flamegraph): reduce DOM & memory load; speed up rendering
    - Reduce the DOM load by removing all redundant bar/node elements. Use a minimal structure.
    - Use event delegation instead of separate events for each bar/node. This should also reduce memory load.
    - ~~Use `clip-path` instead of `transform:scaleX`. Both are hardware-accelerated. The `clip-path` is not animated though which means that when zooming out the bars will have a fixed width. This, however, **speeds up the rendering x5-x6** on zoom. The `transform:translate` positioning is still animated though, so overall the UX and animations don't look vastly different.~~  The rendering is sped up. There are no more long tasks. will-change is employed.
    - The `ResizeObserver` used for the responsiveness is now debounced. The callback uses `element.clientWidth` which triggers reflow. Resizing the window leads to many layout recalculations, i.e. long tasks and lag spikes due to main thread being blocked.
    - Introduce 1px gap among all bars instead of a border (vertical borders didn't seem to work effectively); Fix text overflow issues (noticable on dark background).

--- projects/ngx-flamegraph/src/lib/ngx-flamegraph.module.ts
+++ projects/ngx-flamegraph/src/lib/ngx-flamegraph.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 import { NgxFlamegraphComponent } from './ngx-flamegraph.component';
-import {FlamegraphNodeComponent} from './flamegraph-node/flamegraph-node.component';
-import {FlamegraphComponent} from './flamegraph/flamegraph.component';
-
+import { FlamegraphComponent } from './flamegraph/flamegraph.component';
 
 @NgModule({
-  declarations: [NgxFlamegraphComponent, FlamegraphNodeComponent, FlamegraphComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [NgxFlamegraphComponent]
+  declarations: [NgxFlamegraphComponent, FlamegraphComponent],
+  imports: [CommonModule],
+  exports: [NgxFlamegraphComponent],
 })
-export class NgxFlamegraphModule { }
+export class NgxFlamegraphModule {}
